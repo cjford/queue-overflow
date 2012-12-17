71fb6249c20c06721f2081b328874a3dce3e9543
Connor Ford <cjford@colgate.edu>
2012-12-16 20:36:39 -0500

    Added context_processors to provide login form to navbar

--- app/context_processors.py
+++ app/context_processors.py
@@ -2,3 +2,6 @@ def login_form(request):
   from app.forms import LoginForm
   form = LoginForm(initial={'current_page':request.path})
   return {'login_form':form}
+
+def current_page(request):
+  return {'current_page':request.path}
